76d3aac0f0a483dee8b7b3bf66eb854f95fc0190
Andres Meza <andres_meza@hotmail.es>
2024-07-08 20:23:07 -0500

    fix: arreglo error del componenete ejemplo1

--- ejemplo/src/Main.java
+++ ejemplo/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! vamos a hacer nuestro cambio ajuste antes del commit ");
+        System.out.println("Hello world! vamos a hacer nuestro cambio ajuste antes del commit aca hago un cambio para mi compañero");
     }
 
 }
